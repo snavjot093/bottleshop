89ceb0f8290caebf27baa29c0cc3f65ef6230a9d
Navjot Singh <navjot.singh@kp.org>
2022-09-21 19:25:04 -0700

    Navjot : Clean up after successful deployment

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -22,3 +22,25 @@ export const FirebaseConfig = {
 
 };
 */
+/*// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD74BTxNiZGaQSus2Ur1zuariSOsm-nK-g",
+  authDomain: "bottleshop-c8ea2.firebaseapp.com",
+  projectId: "bottleshop-c8ea2",
+  storageBucket: "bottleshop-c8ea2.appspot.com",
+  messagingSenderId: "60545794046",
+  appId: "1:60545794046:web:844737a0b916f9b3cc8aaf",
+  measurementId: "G-LJKZPP55WJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+*/
